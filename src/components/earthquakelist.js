c386b9ae8f8c6e6ac8e5222d490ea41fcbc39b3e
sgillet1007 <samuel.p.gillet@gmail.com>
2019-02-28 18:57:54 -0700

    convert earthquakelist to functional component

--- src/components/earthquakelist.js
+++ src/components/earthquakelist.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import moment from 'moment'
 
-class EarthquakeList extends Component {
-  render() {
-    const quakeListItems = this.props.renderedEarthquakes.sort((a,b) => b.time - a.time).map(quake => {
-        return (
-          <div key={quake.id}>
-              <h3>{`${moment(quake.time).format('LLL')} @ ${quake.place}`}</h3>
-              <div>
-              <p>{`Magnitude: ${quake.magnitude.toString()}, Lat/Long: ${quake.lat} / ${quake.long}, Depth: ${quake.depth} km`}</p>
-              </div>
-              <hr />
-          </div>
-        )}
-    )
-    return (
-      <div id='quake-list-container'>
-        {quakeListItems}
-      </div>
-    )
-  }
+const EarthquakeList = props => {
+  const quakeListItems = props.renderedEarthquakes.sort((a,b) => b.time - a.time).map(quake => {
+      return (
+        <div key={quake.id}>
+            <h3>{`${moment(quake.time).format('LLL')} @ ${quake.place}`}</h3>
+            <div>
+            <p>{`Magnitude: ${quake.magnitude.toString()}, Lat/Long: ${quake.lat} / ${quake.long}, Depth: ${quake.depth} km`}</p>
+            </div>
+            <hr />
+        </div>
+      )}
+  )
+  return (
+    <div id='quake-list-container'>
+      {quakeListItems}
+    </div>
+  )
 }
 
 export default EarthquakeList
