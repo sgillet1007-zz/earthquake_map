3b01f4df5fd3ab18dc7d5716b3349c2eaa3f5f64
sgillet1007 <samuel.p.gillet@gmail.com>
2019-03-22 23:15:13 -0600

    update dependencies and convert App component to hooks

--- src/App.js
+++ src/App.js
@@ -1,76 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import WorldMap from './components/map';
 import EarthquakeList from './components/earthquakelist';
 
-class App extends Component {
+const App = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      earthquakes: [],
-      renderedEarthquakes: []
-    }
-  }
+const [earthquakes, setEarthquakes] = useState([])
 
-  componentDidMount() {
-    fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson')
-    .then(res => res.json())
-    .then(data => this.setState({ earthquakes: data.features.map(f => {
-		return {
-			id: f.id,
-			place: f.properties.place,
-			type: f.properties.type,
-			time: f.properties.time,
-			magnitude: f.properties.mag,
-			lat: f.geometry.coordinates[1],
-			long: f.geometry.coordinates[0],
-			depth: f.geometry.coordinates[2],
-			url: f.properties.url
-    }}).sort((a, b) => a.time - b.time)
-    })
-    )
+useEffect(() => {
+  fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson')
+  .then(res => res.json())
+  .then(data => setEarthquakes(data.features.map(f => {
+  return {
+    id: f.id,
+    place: f.properties.place,
+    type: f.properties.type,
+    time: f.properties.time,
+    magnitude: f.properties.mag,
+    lat: f.geometry.coordinates[1],
+    long: f.geometry.coordinates[0],
+    depth: f.geometry.coordinates[2],
+    url: f.properties.url
+  }})
+  )
+  )
+}, [])
 
-    this.timerID = setInterval(
-			() => this.tick(),
-			900
-		);
-
-  }
-
-	componentWillUnmount(){
-		clearInterval(this.timerID);
-  }
-  
-  tick =() => {
-		let currentIndex = this.state.renderedEarthquakes.length;
-		if(this.state.renderedEarthquakes.length < this.state.earthquakes.length){
-			const nextQuake = this.state.earthquakes[currentIndex];
-			this.setState({ 
-				renderedEarthquakes: this.state.renderedEarthquakes.concat(nextQuake)
-			})
-		}
-  }
-
-  render() {
-    const worldMapData = {
-      basemap : 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}{r}.png',
-      attribution : '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-      mapCenter : [37, -30],
-      zoomLevel : 2,
-      renderedEarthquakes: this.state.renderedEarthquakes
-    }
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Earthquakes (Past 24 hours)</h1>
-        </header>
-        <WorldMap data={worldMapData} />
-        <EarthquakeList renderedEarthquakes={this.state.renderedEarthquakes} />
-      </div>
-    );
-  }
+const worldMapData = {
+  basemap : 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}{r}.png',
+  attribution : '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+  mapCenter : [37, -30],
+  zoomLevel : 2,
+  renderedEarthquakes: earthquakes
 }
+
+return (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Earthquakes (Past 24 hours)</h1>
+    </header>
+    <WorldMap data={worldMapData} />
+    <EarthquakeList renderedEarthquakes={earthquakes} />
+  </div>
+)}
 
 export default App;
